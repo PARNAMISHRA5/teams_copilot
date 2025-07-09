fd6b3bc2d9caf7deb37cd149ae80860f9bb4d8fa
Radha Tumbre <94734258+radhatumbre@users.noreply.github.com>
2025-07-09 11:28:25 +0530

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { MsalProvider } from "@azure/msal-react";
+import { PublicClientApplication } from "@azure/msal-browser";
+import './index.css' ;
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
+const msalInstance = new PublicClientApplication({
+  auth: {
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}`,
+    redirectUri: process.env.REACT_APP_REDIRECT_URI,
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false,
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <MsalProvider instance={msalInstance}>
     <App />
-  </React.StrictMode>
+  </MsalProvider>
 );
